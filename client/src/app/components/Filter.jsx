4ed809d8b4493a4063ccb9163a21c3c5cc852a1e
Nayem Islam <gangsn490@gmail.com>
2025-08-19 16:57:23 +0600

    feat: update UI and filters per client request

--- client/src/app/components/Filter.jsx
+++ client/src/app/components/Filter.jsx
@@ -23,7 +23,7 @@ const Filter = (props) => {
     classListFilter,
     companyArr,
     setCompanyArr,
-    setClassificationArr,
+    setClassId,
     setMinPrice,
     setMaxPrice,
     setMinDiscount,
@@ -36,205 +36,167 @@ const Filter = (props) => {
   } = props;
 
   const language = localStorage.getItem('language');
-  let dispatch = useDispatch();
   const isArabic = language === 'ar';
 
   // ================================
-  // 📁 HANDLE INPUT
+  // 📁 HANDLE COMPANY
+  // ================================
+  const handleCompanyChange = (id, checked) => {
+    if (checked) {
+      setCompanyArr((prevCompany) => [...prevCompany, id]);
+    } else {
+      setCompanyArr((prevCompany) =>
+        prevCompany.filter((companyId) => companyId !== id)
+      );
+    }
+  };
+  // ================================
+  // 📁 HANDLE CLASS
+  // ================================
+  const handleClassChange = (id, checked) => {
+    if (checked) {
+      setClassId((prev) => [...prev, id]);
+    } else {
+      setClassId((prev) => prev.filter((classId) => classId !== id));
+    }
+  };
+  // ================================
+  // 📁 HANDLE MIN/MAX RANGE
   // ================================
   const handleInput = (e) => {
     setMinPrice(e.minValue);
     setMaxPrice(e.maxValue);
   };
-
+  // ================================
+  // 📁 HANDLE MIN/MAX DISCOUN
+  // ================================
   const handleInputDiscount = (e) => {
     setMinDiscount(e.minValue);
     setMaxDiscount(e.maxValue);
   };
 
   return (
-    <>
-      <div className='left-sidebar-innr'>
-        {/****************** COMPANY CAT: ******************/}
-        <article className='collection-list'>
-          <h6 className='text-capitalize mt-4 mb-4 fw-bold'>Company</h6>
+    <section className='left-sidebar-innr'>
+      {/****************** COMPANY CAT: ******************/}
+      <article className='collection-list'>
+        <h6 className='text-capitalize mt-4 mb-4 fw-bold'>Company</h6>
 
-          <ul
-            className={`mb-4 ${
-              companyList?.length >= 10 ? 'overflow-auto' : ''
-            }`}
-            style={{ maxHeight: companyList?.length >= 10 ? '200px' : 'auto' }}
-          >
-            {companyList?.map((items) => {
-              return (
-                <div key={items?._id}>
-                  <li
-                    className={classNames(
-                      'd-flex align-items-center mb-3 gap-2',
-                      {
-                        'flex-row-reverse': isArabic,
-                      }
-                    )}
-                    key={items.id}
-                  >
-                    <Form.Check
-                      type='checkbox'
-                      id={items._id}
-                      label=''
-                      checked={companyArr.includes(items._id)}
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          setCompanyArr((prevCompany) => [
-                            ...prevCompany,
-                            items._id,
-                          ]);
-                        } else {
-                          setCompanyArr((prevCompany) =>
-                            prevCompany.filter((id) => id !== items._id)
-                          );
-                        }
-                      }}
-                    />
-                    <span className='text-capitalize'>
-                      {checkLanguage(items?.company, items?.arabicCompany)}
-                    </span>
-                  </li>
-                </div>
-              );
-            })}
-          </ul>
-        </article>
+        <ul
+          className={`mb-4 ${companyList?.length >= 10 ? 'overflow-auto' : ''}`}
+          style={{ maxHeight: companyList?.length >= 10 ? '200px' : 'auto' }}
+        >
+          {companyList?.map((items) => {
+            return (
+              <div key={items?._id}>
+                <li
+                  className={classNames(
+                    'd-flex align-items-center mb-3 gap-2',
+                    {
+                      'flex-row-reverse': isArabic,
+                    }
+                  )}
+                  key={items.id}
+                >
+                  <Form.Check
+                    type='checkbox'
+                    id={items._id}
+                    label=''
+                    checked={companyArr.includes(items._id)}
+                    onChange={(e) =>
+                      handleCompanyChange(items._id, e.target.checked)
+                    }
+                  />
+                  <span className='text-capitalize'>
+                    {checkLanguage(items?.company, items?.arabicCompany)}
+                  </span>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </article>
 
-        {/****************** CLASS CAT: ********************/}
-        <article className='collection-list border-top'>
-          <h6 className='text-capitalize mt-4 mb-4 fw-bold'>class</h6>
+      {/****************** CLASS CAT: ********************/}
+      <article className='collection-list border-top'>
+        <h6 className='text-capitalize mt-4 mb-4 fw-bold'>class</h6>
 
-          <ul
-            className={`mb-4 ${
-              classListFilter?.length >= 10 ? 'overflow-auto' : ''
-            }`}
-            style={{
-              maxHeight: classListFilter?.length >= 10 ? '200px' : 'auto',
-            }}
-          >
-            {classListFilter && classListFilter.length > 0
-              ? classListFilter.map((items, i) => (
-                  <li
-                    className={classNames(
-                      'd-flex align-items-center mb-3 gap-2',
-                      {
-                        'flex-row-reverse': isArabic,
-                      }
-                    )}
-                    key={i}
-                  >
-                    <Form.Check
-                      type='checkbox'
-                      id={items._id}
-                      label=''
-                      onChange={(e) => {
-                        if (e.target.checked) {
-                          setClassificationArr((prevClassification) => [
-                            ...prevClassification,
-                            items._id,
-                          ]);
-                        } else {
-                          setClassificationArr((prevClassification) =>
-                            prevClassification.filter((id) => id !== items._id)
-                          );
-                        }
-                      }}
-                    />
-                    <span className='text-capitalize'>
-                      {checkLanguage(items?.name, items?.arbicName)}
-                    </span>
-                  </li>
-                ))
-              : ''}
-          </ul>
-        </article>
+        <ul
+          className={`mb-4 ${
+            classListFilter?.length >= 10 ? 'overflow-auto' : ''
+          }`}
+          style={{
+            maxHeight: classListFilter?.length >= 10 ? '200px' : 'auto',
+          }}
+        >
+          {classListFilter && classListFilter.length > 0
+            ? classListFilter.map((items, i) => (
+                <li
+                  className={classNames(
+                    'd-flex align-items-center mb-3 gap-2',
+                    {
+                      'flex-row-reverse': isArabic,
+                    }
+                  )}
+                  key={i}
+                >
+                  <Form.Check
+                    type='checkbox'
+                    id={items._id}
+                    label=''
+                    onChange={(e) =>
+                      handleClassChange(items._id, e.target.checked)
+                    }
+                  />
+                  <span className='text-capitalize'>
+                    {checkLanguage(items?.name, items?.arbicName)}
+                  </span>
+                </li>
+              ))
+            : ''}
+        </ul>
+      </article>
 
-        {/****************** PRIZE RANGE ***************/}
-        <article className='border-top collection-list'>
-          <h6 className='text-capitalize mt-4 mb-4 fw-bold'>
-            Price range{' '}
-            {` (${
-              formatCurrency('', selectedCountry) +
-              ' ' +
-              minPrice +
-              ' - ' +
-              formatCurrency('', selectedCountry) +
-              ' ' +
-              maxPrice
-            })`}
-          </h6>
-          <MultiRangeSlider
-            min={0}
-            max={1000}
-            step={1}
-            minValue={minPrice}
-            maxValue={maxPrice}
-            onInput={(e) => {
-              handleInput(e);
-            }}
-          />
-        </article>
+      {/****************** PRIZE RANGE ***************/}
+      <article className='collection-list border-top'>
+        <h6 className='text-capitalize mt-4 mb-4 fw-bold'>
+          Price range{' '}
+          {` (${
+            formatCurrency('', selectedCountry) +
+            ' ' +
+            minPrice +
+            ' - ' +
+            formatCurrency('', selectedCountry) +
+            ' ' +
+            maxPrice
+          })`}
+        </h6>
+        <MultiRangeSlider
+          min={0}
+          max={1000}
+          step={1}
+          minValue={minPrice}
+          maxValue={maxPrice}
+          onInput={(e) => {
+            handleInput(e);
+          }}
+        />
+      </article>
 
-        {/****************** DISCOUNT RANGE ******************/}
-        <article className='collection-list border-top mb-3'>
-          <h6 className='text-capitalize mt-4 mb-4 fw-bold'>
-            Discount range{' '}
-            {` (${minDiscount + '%' + ' - ' + maxDiscount + '%'})`}
-          </h6>
-          <MultiRangeSlider
-            min={0}
-            max={100}
-            step={1}
-            minValue={minDiscount}
-            maxValue={maxDiscount}
-            onInput={(e) => handleInputDiscount(e)}
-          />
-        </article>
-
-        {/****************** APPLY BUTTON **************/}
-        {/* <div className='d-flex gap-2 justify-content-center'>
-          <button
-            className='btn btn-primary'
-            title='Filter'
-            onClick={(e) => {
-              e.preventDefault();
-              refetch();
-            }}
-          >
-            Apply
-          </button>
-
-          <button
-            className='btn btn-outline-secondary'
-            title='Reset Filter'
-            onMouseUpCapture={(e) => {
-              e.preventDefault();
-              setCategoryArr([]);
-              setSubCategoryArr([]);
-              setCompanyArr([]);
-              setClassificationArr([]);
-              dispatch(searchQuery(''));
-              setSearch('');
-              setMinPrice(0);
-              setMaxPrice(100000);
-              setMaxDiscount(100);
-              setMinDiscount(0);
-
-              setTimeout(() => {
-                refetch();
-              }, 100);
-            }}
-          >
-            Clear
-          </button>
-        </div> */}
-      </div>
-    </>
+      {/****************** DISCOUNT RANGE ******************/}
+      <article className='collection-list border-top mb-3'>
+        <h6 className='text-capitalize mt-4 mb-4 fw-bold'>
+          Discount range {` (${minDiscount + '%' + ' - ' + maxDiscount + '%'})`}
+        </h6>
+        <MultiRangeSlider
+          min={0}
+          max={100}
+          step={1}
+          minValue={minDiscount}
+          maxValue={maxDiscount}
+          onInput={(e) => handleInputDiscount(e)}
+        />
+      </article>
+    </section>
   );
 };
 
